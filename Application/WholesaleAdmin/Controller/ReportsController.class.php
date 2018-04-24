f57bc175e96ee5f2ba21af56a40294a290c36b67
txw <546578965@qq.com>
2018-04-25 00:03:11 +0800

    报价单页面搭建

--- Application/WholesaleAdmin/Controller/ReportsController.class.php
+++ Application/WholesaleAdmin/Controller/ReportsController.class.php
@@ -14,6 +14,15 @@ use Think\Controller;
 
 class ReportsController extends BaseController
 {
+    /**
+     * 报价单路由
+     */
+    public function quotation(){
+        
+        $this->display();
+
+    }
+
     public function daySaleDetails()
     {
         $wid = getWid();
